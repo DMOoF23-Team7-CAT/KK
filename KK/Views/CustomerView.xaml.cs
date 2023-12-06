29a6548452155699bd82e23ac2b050e19fbe8ea5
Trygvemb <126854646+Trygvemb@users.noreply.github.com>
2023-12-06 10:58:23 +0700

    Customer and start View Setups

--- KK/Views/CustomerView.xaml.cs
+++ KK/Views/CustomerView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KK.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,81 @@ namespace KK.Views
     /// </summary>
     public partial class CustomerView : UserControl
     {
+        private CustomerViewModel customerVM = new CustomerViewModel();
+
         public CustomerView()
         {
+            DataContext = customerVM;
             InitializeComponent();
+        }
+
+        private void BorderYear_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (bb_YearMembership.BorderThickness == new Thickness(0, 0, 4, 4))
+            {
+                bb_YearMembership.BorderThickness = new Thickness(3, 3, 3, 3);
+                bb_YearMembership.BorderBrush = new SolidColorBrush(Colors.Beige);
+
+                bb_QuaterMembership.BorderThickness = new Thickness(0, 0, 4, 4);
+                bb_QuaterMembership.BorderBrush = new SolidColorBrush(Colors.Gray);
+                (bb_QuaterMembership.BorderBrush as SolidColorBrush).Opacity = 0.2;
+
+                bb_MonthMembership.BorderThickness = new Thickness(0, 0, 4, 4);
+                bb_MonthMembership.BorderBrush = new SolidColorBrush(Colors.Gray);
+                (bb_MonthMembership.BorderBrush as SolidColorBrush).Opacity = 0.2;
+            }
+            else
+            {
+                bb_YearMembership.BorderThickness = new Thickness(0, 0, 4, 4);
+                bb_YearMembership.BorderBrush = new SolidColorBrush(Colors.Gray);
+                (bb_YearMembership.BorderBrush as SolidColorBrush).Opacity = 0.2;
+            }
+        }
+
+        private void BorderQuarter_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (bb_QuaterMembership.BorderThickness == new Thickness(0, 0, 4, 4))
+            {
+                bb_QuaterMembership.BorderThickness = new Thickness(3, 3, 3, 3);
+                bb_QuaterMembership.BorderBrush = new SolidColorBrush(Colors.Beige);
+
+                bb_YearMembership.BorderThickness = new Thickness(0, 0, 4, 4);
+                bb_YearMembership.BorderBrush = new SolidColorBrush(Colors.Gray);
+                (bb_YearMembership.BorderBrush as SolidColorBrush).Opacity = 0.2;
+
+                bb_MonthMembership.BorderThickness = new Thickness(0, 0, 4, 4);
+                bb_MonthMembership.BorderBrush = new SolidColorBrush(Colors.Gray);
+                (bb_MonthMembership.BorderBrush as SolidColorBrush).Opacity = 0.2;
+            }
+            else
+            {
+                bb_QuaterMembership.BorderThickness = new Thickness(0, 0, 4, 4);
+                bb_QuaterMembership.BorderBrush = new SolidColorBrush(Colors.Gray);
+                (bb_QuaterMembership.BorderBrush as SolidColorBrush).Opacity = 0.2;
+            }
+        }
+
+        private void BorderMonth_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (bb_MonthMembership.BorderThickness == new Thickness(0, 0, 4, 4))
+            {
+                bb_MonthMembership.BorderThickness = new Thickness(3, 3, 3, 3);
+                bb_MonthMembership.BorderBrush = new SolidColorBrush(Colors.Beige);
+
+                bb_QuaterMembership.BorderThickness = new Thickness(0, 0, 4, 4);
+                bb_QuaterMembership.BorderBrush = new SolidColorBrush(Colors.Gray);
+                (bb_QuaterMembership.BorderBrush as SolidColorBrush).Opacity = 0.2;
+
+                bb_YearMembership.BorderThickness = new Thickness(0, 0, 4, 4);
+                bb_YearMembership.BorderBrush = new SolidColorBrush(Colors.Gray);
+                (bb_YearMembership.BorderBrush as SolidColorBrush).Opacity = 0.2;
+            }
+            else
+            {
+                bb_MonthMembership.BorderThickness = new Thickness(0, 0, 4, 4);
+                bb_MonthMembership.BorderBrush = new SolidColorBrush(Colors.Gray);
+                (bb_MonthMembership.BorderBrush as SolidColorBrush).Opacity = 0.2;
+            }
         }
     }
 }
