039dc0ac7a91deaf771fe1ab7172e8debe508f74
Adam Eleraky <adam_eleraky@hotmail.com>
2023-12-18 19:20:10 +0100

    Changes made

--- KK/Views/MainWindow.xaml.cs
+++ KK/Views/MainWindow.xaml.cs
@@ -21,24 +21,6 @@ namespace KK.Views
 
         private bool IsMaximized = false;
 
-        private void btn_Maximize_Click(object sender, RoutedEventArgs e)
-        {
-            if (IsMaximized)
-            {
-                this.WindowState = WindowState.Normal;
-                this.Height = 784;
-                this.Width = 1366;
-
-                IsMaximized = false;
-            }
-            else
-            {
-                this.WindowState = WindowState.Maximized;
-
-                IsMaximized = true;
-            }
-        }
-
         private void btn_Minimize_Click(object sender, RoutedEventArgs e)
         {
             WindowState = WindowState.Minimized;
@@ -55,27 +37,7 @@ namespace KK.Views
             if (e.ChangedButton == MouseButton.Left)
                 this.DragMove();
         }
-
-        private void Border_DoubleMouseLeftButtonClick(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ClickCount == 2)
-            {
-                if (IsMaximized)
-                {
-                    this.WindowState = WindowState.Normal;
-                    this.Height = 784;
-                    this.Width = 1366;
-
-                    IsMaximized = false;
-                }
-                else
-                {
-                    this.WindowState = WindowState.Maximized;
-
-                    IsMaximized = true;
-                }
-            }
-        }
+      
 
     }
 }
